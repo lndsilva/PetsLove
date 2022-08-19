99e7f9fe91ddb893587d80504765fd88c908c27f
lndsilva <lndsilva@hotmail.com>
2022-08-18 22:11:06 -0300

    trabalhando com relative layout

---
+++ app/src/main/java/br/com/etecia/petslove/SplashActivity.java
@@ -0,0 +1,16 @@
+package br.com.etecia.petslove;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
